0a3322b84e2de130f7e3a9ba10c97341cbe3f3ea
Surendra Kumar <skumarlabs@users.noreply.github.com>
2017-09-15 03:39:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ Created on Thu Sep 14 23:21:52 2017
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
-from tfrecords_converter import TfConverter
+from tfrecord_converter import TfConverter
 from batch_generator import BatchGenerator
 
 # shuffle_data = True  # shuffle the addresses before saving
